7e0258cc839f4be712002b0d73478bb4c4e80d83
Maksim Terentev <maxim.i.terentev@gmail.com>
2024-03-16 00:15:41 +0300

    optimezed UI elements creation

--- interface.py
+++ interface.py
@@ -3,6 +3,7 @@ import dearpygui.dearpygui as dpg
 c, si, mn, cr, ni, cu, mo, v = 0, 0, 0, 0, 0, 0, 0, 0
 composition = c, si, mn, cr, ni, cu, mo, v
 element_names = ['C', 'Si', 'Mn', 'Cr', 'Ni', 'Cu', 'Mo', 'V']
+element_max_values = [0.8, 1, 1.5, 2, 3, 0.5, 1, 0.2]
 
 def get_composition(ui_elements: tuple) -> tuple[float]:
     """_summary_
@@ -18,15 +19,17 @@ def get_composition(ui_elements: tuple) -> tuple[float]:
 
 def calc_btn_action():
     # changing the 'composition' from the global scope
-    composition = get_composition(composition_ui_elements)
+    composition = get_composition(input_elements)
     # temporary
     for element, content in zip(element_names, composition):
         print(element, content)
+    print(input_elements)
+    print(type(input_elements[0]))
 
 def reset_btn_action():
     print('reset_btn_action')
-    for ui_el in composition_ui_elements:
-        dpg.set_value(ui_el, 0)
+    for el in input_elements:
+        dpg.set_value(el, 0)
 
 
 primary_window_tag = 'Primary window'
@@ -36,26 +39,11 @@ dpg.create_context()
 
 with dpg.window(tag=primary_window_tag):
     dpg.add_text("Continious cooling transformation diagram calculator")
-    input_c = dpg.add_input_float(
-        label='C', min_value=0, max_value=0.8, step=0.01)
-    input_si = dpg.add_input_float(
-        label='Si', min_value=0, max_value=1, step=0.01)
-    input_mn = dpg.add_input_float(
-        label='Mn', min_value=0, max_value=1.5, step=0.01)
-    input_cr = dpg.add_input_float(
-        label='Cr', min_value=0, max_value=2, step=0.01)
-    input_ni = dpg.add_input_float(
-        label='Ni', min_value=0, max_value=3, step=0.01)
-    input_cu = dpg.add_input_float(
-        label='Cu', min_value=0, max_value=0.5, step=0.01)
-    input_mo = dpg.add_input_float(
-        label='Mo', min_value=0, max_value=1, step=0.01)
-    input_v = dpg.add_input_float(
-        label='V', min_value=0, max_value=0.2, step=0.01)
-    composition_ui_elements = (
-        input_c, input_si, input_mn, input_cr,
-        input_ni, input_cu, input_mo, input_v
-    )
+    input_elements = [
+        dpg.add_input_float(label=name, tag=name, min_value=0,
+                            max_value=max_val, step=0.01)
+        for name, max_val in zip(element_names, element_max_values)
+    ]
 
     # Create the actions for the 'Calculate' button
     calc_btn = dpg.add_button(label='Calculate')
